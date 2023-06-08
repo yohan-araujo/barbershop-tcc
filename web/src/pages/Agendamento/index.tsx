83bab36ccab4bc08d2ebf30121b5cb19cbeb2223
André Santos <andred.santos1842@gmail.com>
2023-06-08 00:08:42 -0300

    primeiro commit

--- web/src/pages/Agendamento/index.tsx
+++ web/src/pages/Agendamento/index.tsx
@@ -1,17 +1,17 @@
 // Importacoes
 
-import ButtonPadrao from 'components/ButtonPadrao';
-import ListaCards from './ListaCards';
-import TabelaServicos from './TabelaServicos';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { IProfissional } from 'types/IProfissional';
-import { IServico } from 'types/IServico';
-import DatePicker from 'react-datepicker';
-import TimePicker from 'react-time-picker';
-import { format } from 'date-fns';
-import 'react-datepicker/dist/react-datepicker.css';
-import 'react-time-picker/dist/TimePicker.css';
+import ButtonPadrao from "components/ButtonPadrao";
+import ListaCards from "./ListaCards";
+import TabelaServicos from "./TabelaServicos";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { IProfissional } from "types/IProfissional";
+import { IServico } from "types/IServico";
+import DatePicker from "react-datepicker";
+import TimePicker from "react-time-picker";
+import { format } from "date-fns";
+import "react-datepicker/dist/react-datepicker.css";
+import "react-time-picker/dist/TimePicker.css";
 
 const Agendamento = () => {
   // useStates ou Variaveis
@@ -30,7 +30,7 @@ const Agendamento = () => {
   //Trazendo informação do banco
   useEffect(() => {
     axios
-      .get<IProfissional[]>('http://localhost:3001/api/getProfissionais')
+      .get<IProfissional[]>("http://localhost:3001/api/getProfissionais")
       .then((response) => {
         setListaProfissionais(response.data);
       });
@@ -38,7 +38,7 @@ const Agendamento = () => {
 
   useEffect(() => {
     axios
-      .get<IServico[]>('http://localhost:3001/api/getServicos')
+      .get<IServico[]>("http://localhost:3001/api/getServicos")
       .then((response) => {
         setListaServicos(response.data);
       });
@@ -86,92 +86,103 @@ const Agendamento = () => {
       profissionalSelecionado &&
       servicoSelecionado
     ) {
-      const dataFormatada = format(dataSelecionada, 'yyyy-MM-dd');
+      const dataFormatada = format(dataSelecionada, "yyyy-MM-dd");
       axios
-        .post('http://localhost:3001/api/insertAgendamento', {
+        .post("http://localhost:3001/api/insertAgendamento", {
           data: dataFormatada,
           hora: horaSelecionada,
           profissionalID: profissionalSelecionado.pro_id,
           servicoID: servicoSelecionado.ser_id,
         })
         .then((response) => {
-          console.log('Agendamento inserido com sucesso!');
+          console.log("Agendamento inserido com sucesso!");
         })
         .catch((error) => {
-          console.error('Erro ao inserir agendamento:', error);
+          console.error("Erro ao inserir agendamento:", error);
         });
     }
   };
 
   return (
-    <section className="container mx-auto px-4 py-8">
-      <div className="max-w-2xl mx-auto bg-slate-300">
-        <div className="flex items-center justify-between mb-4">
-          <div className="text-sm">Etapa {etapaAtual} de 3</div>
+    <section className="bg-age">
+      
+      <div>
+        <div className="flex justify-start items-start p-4 gap-3 mb-8">
+          <div className="w-12 h-12 bg-gray-300 flex items-center justify-center rounded-full">
+            <div
+              className={`w-8 h-8 rounded-full ${
+                etapaAtual >= 1 ? "bg-blue-500" : "bg-gray-300"
+              }`}
+            ></div>
+          </div>
+
+          <div className="w-12 h-12 bg-gray-300 flex items-center justify-center rounded-full">
+            <div
+              className={`w-8 h-8 rounded-full ${
+                etapaAtual >= 2 ? "bg-blue-500" : "bg-gray-300"
+              }`}
+            ></div>
+          </div>
+          <div className="w-12 h-12 bg-gray-300 flex items-center justify-center rounded-full">
+            <div
+              className={`w-8 h-8 rounded-full ${
+                etapaAtual >= 3 ? "bg-blue-500" : "bg-gray-300"
+              }`}
+            ></div>
+          </div>
         </div>
 
-        <div className="flex justify-between mb-8">
-          <div
-            className={`w-1/3 h-4 rounded-full ${
-              etapaAtual >= 1 ? 'bg-blue-500' : 'bg-gray-300'
-            }`}
-          ></div>
-          <div
-            className={`w-1/3 h-4 rounded-full ${
-              etapaAtual >= 2 ? 'bg-blue-500' : 'bg-gray-300'
-            }`}
-          ></div>
-          <div
-            className={`w-1/3 h-4 rounded-full ${
-              etapaAtual >= 3 ? 'bg-blue-500' : 'bg-gray-300'
-            }`}
-          ></div>
+        <div className="flex justify-center w-screen">
+          <form
+            onSubmit={handleSubmit}
+            className="flex ite rounded-3xl container h-[38rem] 
+            w-[82rem] mb-10  bg-slate-400"
+          >
+            {etapaAtual === 1 && (
+              <>
+                <ListaCards
+                  profissionais={listaProfissionais}
+                  onProfissionalSelecionado={handleProfissionalSelecionado}
+                />
+
+                <ButtonPadrao texto="Proximo" onClick={handleProximaEtapa} />
+              </>
+            )}
+
+            {etapaAtual === 2 && (
+              <>
+                <TabelaServicos
+                  servicos={listaServicos}
+                  onServicoSelecionado={handleServicoSelecionado}
+                />
+                <ButtonPadrao texto="Voltar" onClick={handleEtapaAnterior} />
+                <ButtonPadrao texto="Proximo" onClick={handleProximaEtapa} />
+              </>
+            )}
+
+            {etapaAtual === 3 && (
+              <>
+                <label>Data</label>
+                <DatePicker
+                  selected={dataSelecionada}
+                  onChange={handleDateChange}
+                  dateFormat="dd/MM/yyyy"
+                />
+
+                <label>Hora:</label>
+                <TimePicker
+                  value={horaSelecionada}
+                  onChange={handleTimeChange}
+                  disableClock={true}
+                />
+                <ButtonPadrao texto="Voltar" onClick={handleEtapaAnterior} />
+                <ButtonPadrao texto="Agendar!" tipo="submit" />
+              </>
+            )}
+          </form>
         </div>
-
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          {etapaAtual === 1 && (
-            <>
-              <ListaCards
-                profissionais={listaProfissionais}
-                onProfissionalSelecionado={handleProfissionalSelecionado}
-              />
-
-              <ButtonPadrao texto="Proximo" onClick={handleProximaEtapa} />
-            </>
-          )}
-
-          {etapaAtual === 2 && (
-            <>
-              <TabelaServicos
-                servicos={listaServicos}
-                onServicoSelecionado={handleServicoSelecionado}
-              />
-              <ButtonPadrao texto="Voltar" onClick={handleEtapaAnterior} />
-              <ButtonPadrao texto="Proximo" onClick={handleProximaEtapa} />
-            </>
-          )}
-
-          {etapaAtual === 3 && (
-            <>
-              <label>Data</label>
-              <DatePicker
-                selected={dataSelecionada}
-                onChange={handleDateChange}
-                dateFormat="dd/MM/yyyy"
-              />
-
-              <label>Hora:</label>
-              <TimePicker
-                value={horaSelecionada}
-                onChange={handleTimeChange}
-                disableClock={true}
-              />
-              <ButtonPadrao texto="Voltar" onClick={handleEtapaAnterior} />
-              <ButtonPadrao texto="Agendar!" tipo="submit" />
-            </>
-          )}
-        </form>
       </div>
+      
     </section>
   );
 };
