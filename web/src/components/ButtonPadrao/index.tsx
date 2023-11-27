0b1f157ff1d7f69dd089380cf4a13b48f4720e86
André Santos <andred.santos1842@gmail.com>
2023-11-26 23:31:32 -0300

    Ajustando os botoes do cadastro profissional e adicionando fundo preto no grafico

--- web/src/components/ButtonPadrao/index.tsx
+++ web/src/components/ButtonPadrao/index.tsx
@@ -3,16 +3,18 @@ interface PropsButton {
   onClick?: () => void;
   tipo?: "submit";
   outline?: boolean;
+  tamanho?: string;
 }
 
-const ButtonPadrao = ({ texto, onClick, tipo, outline }: PropsButton) => {
+const ButtonPadrao = ({ texto, onClick, tipo, tamanho, outline }: PropsButton) => {
   return (
     <button
-      className={` text-white font-medium text-xl py-2 px-12 font-face-montserrat ${
-        outline
+      className={` text-white font-medium text-xl py-2 px-12 font-face-montserrat ${outline
           ? "border border-[#E29C31] hover:bg-[#E29C31]"
           : "bg-[#E29C31] bg-transparent border border-[#E29C31]"
-      } `}
+        } 
+      ${tamanho || ""}`
+      }
       onClick={onClick}
       type={tipo}
     >
