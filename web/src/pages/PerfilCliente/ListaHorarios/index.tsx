46dc1cd37f72c84fcac4902483dff8e3731de371
Yohan Araujo <yohanneves@outlook.pt>
2023-11-26 19:56:38 -0800

    Refazendo a lista de agendamentos do perfil cliente

---
+++ web/src/pages/PerfilCliente/ListaHorarios/index.tsx
@@ -0,0 +1,59 @@
+import { IAgendamento } from 'types/IAgendamento';
+
+interface ListaHorariosProps {
+  agendamentos: IAgendamento[];
+}
+
+const ListaHorarios = ({ agendamentos }: ListaHorariosProps) => {
+  return (
+    <div className="relative">
+      <div className="absolute top-2 left-8 h-[13rem] w-[40.5rem] border-2 border-[#E29C31]"></div>
+      <div className="bg-black shadow-inner p-5 relative z-10">
+        <div className="text-white">
+          <div className="grid grid-cols-5 gap-0 text-center">
+            <div className="font-face-montserrat px-8 py-2">Tipo</div>
+            <div className="font-face-montserrat px-4 py-2">Data</div>
+            <div className="font-face-montserrat px-4 py-2">Hora</div>
+            <div className="font-face-montserrat px-4 py-2">Profissional</div>
+            <div className="font-face-montserrat px-4 py-2">Preço</div>
+          </div>
+          {agendamentos.map((horario) => {
+            return (
+              <div
+                key={horario.age_id}
+                className={
+                  horario.age_status
+                    ? 'highlighted-row grid grid-cols-5 gap-0'
+                    : 'grid grid-cols-5 gap-0'
+                }
+              >
+                <div className="flex flex-row font-face-montserrat text-center px-2 py-2">
+                  <div className="inline-flex items-center justify-center h-11 w-11 rounded-full bg-white mr-2">
+                    icon
+                  </div>
+                  <div className="font-face-montserrat mt-2">
+                    {horario.ser_tipo}
+                  </div>
+                </div>
+                <div className="font-face-montserrat text-center py-2 mt-2">
+                  {new Date(horario.age_data).toLocaleDateString('pt-BR')}
+                </div>
+                <div className="font-face-montserrat text-center py-2 mt-2">
+                  {horario.age_hora.substring(0, 5)}
+                </div>
+                <div className="font-face-montserrat text-center py-2 mt-2">
+                  {horario.usu_nomeCompleto}
+                </div>
+                <div className="font-face-montserrat text-center py-2 mt-2">
+                  R$ {horario.ser_preco.toFixed(2)}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListaHorarios;
--- web/src/pages/PerfilCliente/index.tsx
+++ web/src/pages/PerfilCliente/index.tsx
@@ -1,11 +1,8 @@
 import ButtonPadrao from 'components/ButtonPadrao';
-import { Edit } from 'lucide-react';
-import ListaHorariosAtivos from './ListaHorariosAtivos';
+import ListaHorariosAtivos from './ListaHorarios';
 import { useEffect, useState } from 'react';
 import { IAgendamento } from 'types/IAgendamento';
 import axios from 'axios';
-import ListaHorariosInativos from './ListaHorariosInativos';
-import { Link } from 'react-router-dom';
 import CartaoFidelidade from './CartaoFidelidade';
 import { ICartaoFidelidade } from 'types/ICartaoFidelidade';
 
@@ -13,9 +10,7 @@ const PerfilCliente = () => {
   const [listaAgendamentosAtivos, setListaAgendamentosAtivos] = useState<
     IAgendamento[]
   >([]);
-  const [listaAgendamentosInativos, setListaAgendamentosInativos] = useState<
-    IAgendamento[]
-  >([]);
+
   const [cartoes, setCartoes] = useState<ICartaoFidelidade[]>([]);
   const [fotoUsuario, setFotoUsuario] = useState('');
 
@@ -26,24 +21,12 @@ const PerfilCliente = () => {
   useEffect(() => {
     axios
       .get<IAgendamento[]>(
-        `http://localhost:3001/api/getAgendamentosAtivos/${sessionStorage.getItem(
+        `http://localhost:3001/api/getAgendamentosAtivosInativos/${sessionStorage.getItem(
           'clienteID'
         )}`
       )
       .then((response) => {
         setListaAgendamentosAtivos(response.data);
-      });
-  }, []);
-
-  useEffect(() => {
-    axios
-      .get<IAgendamento[]>(
-        `http://localhost:3001/api/getAgendamentosInativos/${sessionStorage.getItem(
-          'clienteID'
-        )}`
-      )
-      .then((response) => {
-        setListaAgendamentosInativos(response.data);
       });
   }, []);
 
@@ -99,14 +82,19 @@ const PerfilCliente = () => {
                 <span className="text-white text-4xl font-semibold font-face-montserrat">
                   Horários ativos
                 </span>
-                <div className="w-10 h-10 rounded-full bg-zinc-600 flex justify-center mt-1 ml-3 text-white">
-                  <Link to="#" className="mt-2 ml-1">
-                    <Edit />
-                  </Link>
-                </div>
               </div>
               <div className="flex mt-12 ml-">
-                <div className="flex ">
+                <div className="flex flex-col ">
+                  <div className="flex flex-row gap-2">
+                    <div className="w-4 h-4 bg-[#E29C31]"></div>
+                    <h1 className="text-white font-face-montserrat">
+                      Horarios ativos
+                    </h1>
+                    <div className="w-4 h-4 bg-black"></div>
+                    <h1 className="text-white font-face-montserrat">
+                      Horarios inativos
+                    </h1>
+                  </div>
                   {listaAgendamentosAtivos.length > 0 ? (
                     <ListaHorariosAtivos
                       agendamentos={listaAgendamentosAtivos}
@@ -126,21 +114,6 @@ const PerfilCliente = () => {
                 <span className="text-white text-4xl font-semibold font-face-montserrat">
                   Histórico
                 </span>
-                <div className="flex flex-col mt-12 ml-4">
-                  <div className="flex">
-                    {listaAgendamentosInativos.length > 0 ? (
-                      <ListaHorariosInativos
-                        agendamentos={listaAgendamentosInativos}
-                      />
-                    ) : (
-                      <div>
-                        <p className="flex justify-center font-bold font-face-montserrat text-4xl text-white text-center">
-                          Nenhum Agendamento registrado.
-                        </p>
-                      </div>
-                    )}
-                  </div>
-                </div>
               </div>
               <div className="w-[28rem] mt-4"></div>
             </div>
